660a609059f55b14160d4d5b80617554089f985e
cryptods8 <dusoo.s+ds8@gmail.com>
2025-01-14 17:25:32 +0100

    styling

--- app/app/leaderboard/leaderboard-nav.tsx
+++ app/app/leaderboard/leaderboard-nav.tsx
@@ -12,8 +12,8 @@ function NavLink({
   return (
     <Link
       href={href}
-      className={`text-center rounded-full font-semibold px-4 py-2.5 text-sm hover:bg-primary-200 hover:text-primary-800/90 transition duration-150 ease-in-out ${
-        active ? "bg-primary-800 text-white" : "text-primary-800/90"
+      className={`text-center rounded-full font-semibold px-4 py-2.5 text-sm transition duration-150 ease-in-out ${
+        active ? "bg-primary-800 text-white" : "text-primary-800/90 hover:bg-primary-200 hover:text-primary-800/90"
       }`}
     >
       {children}
