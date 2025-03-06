d8f1cdad496986f13f067e11afa5831c6fb9d008
cryptods8 <dusoo.s+ds8@gmail.com>
2025-03-06 08:19:17 +0100

    notifs with yesterdays word

--- app/api/frames/v2/notifications/process/route.ts
+++ app/api/frames/v2/notifications/process/route.ts
@@ -1,13 +1,27 @@
 import { NextRequest, NextResponse } from "next/server";
 import { sendFrameNotifications } from "@/app/utils/send-frame-notifications";
-import { getDailyGameKey } from "@/app/game/game-utils";
+import { addDaysToDate, getDailyGameKey } from "@/app/game/game-utils";
 import { pgDb } from "@/app/db/pg/pg-db";
 import { FrameNotificationDetails } from "@farcaster/frame-sdk";
 import { isPro } from "@/app/constants";
+import { GameIdentityProvider } from "@/app/game/game-repository";
+import { getWordForUserGameKey } from "@/app/game/game-service";
 
 export const dynamic = "force-dynamic";
 export const runtime = "nodejs";
 export const maxDuration = 200;
+
+async function getYesterdaysWord(ip: GameIdentityProvider) {
+  const yesterday = addDaysToDate(new Date(), -1);
+  const gameKey = getDailyGameKey(yesterday);
+  const { word } = await getWordForUserGameKey({
+    gameKey,
+    userId: "unimportant",
+    identityProvider: ip,
+    isDaily: true,
+  });
+  return word;
+}
 
 export async function GET(req: NextRequest) {
   if (
@@ -48,11 +62,14 @@ export async function GET(req: NextRequest) {
       )
       .execute();
     const name = isPro ? "Framedl PRO" : "Framedl";
+    const yesterdayWord = await getYesterdaysWord("fc");
     const title =
       type === "new" ? `New daily ${name}` : `Do not miss your daily ${name}`;
     const body =
       type === "new"
-        ? "Your daily game is ready!"
+        ? isPro
+          ? "Your daily game is ready!"
+          : `${yesterdayWord.toUpperCase()} stumped many! Can you crack today's word?`
         : "You have last 2 hours to play your daily game!";
     // batch by 100
     console.log("to notify", toNotify.length);
@@ -68,7 +85,11 @@ export async function GET(req: NextRequest) {
           }
           return acc;
         }, [] as { fid: number; notificationDetails: FrameNotificationDetails }[]);
-      const notificationResult = await sendFrameNotifications({ recipients, title, body });
+      const notificationResult = await sendFrameNotifications({
+        recipients,
+        title,
+        body,
+      });
       console.log("sent", recipients.length, notificationResult);
       // if (i + 100 < toNotify.length) {
       //   await new Promise((resolve) => setTimeout(resolve, 50));
